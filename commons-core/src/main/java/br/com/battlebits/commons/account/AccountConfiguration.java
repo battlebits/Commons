5429eb2b22f29e4f9f429a937e9ef7ae7de80e52
Gustavo Inacio <incio.gusta@gmail.com>
2019-07-31 19:37:36 -0300

    Adding attributes

--- commons-core/src/main/java/br/com/battlebits/commons/account/AccountConfiguration.java
+++ commons-core/src/main/java/br/com/battlebits/commons/account/AccountConfiguration.java
@@ -1,4 +1,74 @@
 package br.com.battlebits.commons.account;
 
+import br.com.battlebits.commons.account.BattleAccount;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@NoArgsConstructor
 public class AccountConfiguration {
+
+    private boolean ignoreAll = false;
+    private boolean tellEnabled = true;
+    private boolean canPlaySound = true;
+    private boolean alertsEnabled = true;
+    private boolean staffChatEnabled = false;
+    private boolean clanChatEnabled = false;
+    private boolean partyInvites = true;
+    private boolean rankedEnabled = true;
+
+    protected transient BattleAccount account;
+
+    public AccountConfiguration(BattleAccount account) {
+        this.account = account;
+    }
+
+    public void setCanPlaySound(boolean canPlaySound) {
+        if (this.canPlaySound != canPlaySound) {
+            this.canPlaySound = canPlaySound;
+        }
+    }
+
+    public void setClanChatEnabled(boolean clanChatEnabled) {
+        if (this.clanChatEnabled != clanChatEnabled) {
+            this.clanChatEnabled = clanChatEnabled;
+        }
+    }
+
+    public void setIgnoreAll(boolean ignoreAll) {
+        if (this.ignoreAll != ignoreAll) {
+            this.ignoreAll = ignoreAll;
+        }
+    }
+
+    public void setAlertsEnabled(boolean showAlerts) {
+        if (this.alertsEnabled != showAlerts) {
+            this.alertsEnabled = showAlerts;
+        }
+    }
+
+    public void setStaffChatEnabled(boolean staffChatEnabled) {
+        if (this.staffChatEnabled != staffChatEnabled) {
+            this.staffChatEnabled = staffChatEnabled;
+        }
+    }
+
+    public void setTellEnabled(boolean tellEnabled) {
+        if (this.tellEnabled != tellEnabled) {
+            this.tellEnabled = tellEnabled;
+        }
+    }
+
+    public void setPartyReceiveInvite(boolean tellEnabled) {
+        if (this.tellEnabled != tellEnabled) {
+            this.tellEnabled = tellEnabled;
+        }
+    }
+
+    public void setRankedEnabled(boolean rankedEnabled) {
+        if (this.rankedEnabled != rankedEnabled) {
+            this.rankedEnabled = rankedEnabled;
+        }
+    }
 }
--- commons-core/src/main/java/br/com/battlebits/commons/account/punishment/Mute.java
+++ commons-core/src/main/java/br/com/battlebits/commons/account/punishment/Mute.java
@@ -1,4 +1,72 @@
 package br.com.battlebits.commons.account.punishment;
 
+import java.util.UUID;
+
+import br.com.battlebits.commons.core.account.BattlePlayer;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public class Mute {
+
+	private String mutedBy;
+	private UUID mutedByUniqueId;
+	private String mutedIp;
+
+	private String server;
+
+	private long muteTime;
+	private String reason;
+
+	private boolean unmuted;
+	private String unmutedBy;
+	private UUID unmutedByUniqueId;
+	private long unmuteTime;
+
+	private long expire;
+	private long duration;
+
+	public Mute(String mutedBy, String mutedIp, String server, String reason, long duration) {
+		this(mutedBy, null, mutedIp, server, reason, duration);
+	}
+
+	public Mute(String mutedBy, UUID mutedByUuid, String mutedIp, String server, String reason, long expire) {
+		this(mutedBy, mutedByUuid, mutedIp, server, System.currentTimeMillis(), reason, false, null, null, -1, expire, expire - System.currentTimeMillis());
+	}
+
+	public Mute(String mutedBy, String mutedIp, String server, String reason) {
+		this(mutedBy, null, mutedIp, server, reason);
+	}
+
+	public Mute(String mutedBy, UUID mutedByUuid, String mutedIp, String server, String reason) {
+		this(mutedBy, mutedByUuid, mutedIp, server, System.currentTimeMillis(), reason, false, null, null, -1, -1, -1);
+	}
+	
+	public boolean hasExpired() {
+		return expire != -1 && expire < System.currentTimeMillis();
+	}
+
+	public boolean isPermanent() {
+		return expire == -1;
+	}
+
+	public void unmute() {
+		this.unmuted = true;
+		this.unmutedBy = "CONSOLE";
+		this.unmuteTime = System.currentTimeMillis();
+	}
+
+	public void unmute(BattlePlayer unmutePlayer) {
+		unmute(unmutePlayer.getUniqueId(), unmutePlayer.getName());
+	}
+
+	public void unmute(UUID unmuteUuid, String unmuteName) {
+		this.unmuted = true;
+		this.unmutedBy = unmuteName;
+		this.unmutedByUniqueId = unmuteUuid;
+		this.unmuteTime = System.currentTimeMillis();
+	}
+	
+	
 }
