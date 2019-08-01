5f25821182e6719610d982473133c16bf7dce6ec
Chateado <chateadorn@gmail.com>
2019-07-31 23:09:56 -0300

    Disabled command "/me"

--- commons-bukkit/src/main/java/br/com/battlebits/commons/bukkit/listener/PlayerListener.java
+++ commons-bukkit/src/main/java/br/com/battlebits/commons/bukkit/listener/PlayerListener.java
@@ -1,6 +1,22 @@
 package br.com.battlebits.commons.bukkit.listener;
 
+import org.bukkit.ChatColor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 
 public class PlayerListener implements Listener {
+
+
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.NORMAL)
+    public void onPreProcessCommand(PlayerCommandPreprocessEvent event) {
+        if (event.getMessage().toLowerCase().startsWith("/me ")) {
+            event.setCancelled(true);
+        }
+        if (event.getMessage().split(" ")[0].contains(":")) {
+            event.getPlayer().sendMessage(ChatColor.RED + "Você não pode digitar comando com dois pontos (:)");
+            event.setCancelled(true);
+        }
+    }
 }
