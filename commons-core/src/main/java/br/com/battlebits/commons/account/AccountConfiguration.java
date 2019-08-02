74f127e0f944673339e409da44d8d052efad411c
Gustavo Inacio <incio.gusta@gmail.com>
2019-08-01 22:29:15 -0300

    Removed some unused attributes

--- commons-core/src/main/java/br/com/battlebits/commons/account/AccountConfiguration.java
+++ commons-core/src/main/java/br/com/battlebits/commons/account/AccountConfiguration.java
@@ -1,7 +1,7 @@
 package br.com.battlebits.commons.account;
 
-import br.com.battlebits.commons.account.BattleAccount;
-import lombok.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
@@ -9,12 +9,8 @@ public class AccountConfiguration {
 
     private boolean ignoreAll = false;
     private boolean tellEnabled = true;
-    private boolean canPlaySound = true;
-    private boolean alertsEnabled = true;
     private boolean staffChatEnabled = false;
-    private boolean clanChatEnabled = false;
     private boolean partyInvites = true;
-    private boolean rankedEnabled = true;
 
     protected transient BattleAccount account;
 
@@ -22,27 +18,9 @@ public class AccountConfiguration {
         this.account = account;
     }
 
-    public void setCanPlaySound(boolean canPlaySound) {
-        if (this.canPlaySound != canPlaySound) {
-            this.canPlaySound = canPlaySound;
-        }
-    }
-
-    public void setClanChatEnabled(boolean clanChatEnabled) {
-        if (this.clanChatEnabled != clanChatEnabled) {
-            this.clanChatEnabled = clanChatEnabled;
-        }
-    }
-
     public void setIgnoreAll(boolean ignoreAll) {
         if (this.ignoreAll != ignoreAll) {
             this.ignoreAll = ignoreAll;
-        }
-    }
-
-    public void setAlertsEnabled(boolean showAlerts) {
-        if (this.alertsEnabled != showAlerts) {
-            this.alertsEnabled = showAlerts;
         }
     }
 
@@ -64,9 +42,4 @@ public class AccountConfiguration {
         }
     }
 
-    public void setRankedEnabled(boolean rankedEnabled) {
-        if (this.rankedEnabled != rankedEnabled) {
-            this.rankedEnabled = rankedEnabled;
-        }
-    }
 }
