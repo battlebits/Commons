8ad396865c989dfaca2cb11c0db2b5405d80504e
Chateado <chateadorn@gmail.com>
2019-08-03 10:56:02 -0300

    Added BukkitParty methods

--- commons-bukkit/src/main/java/br/com/battlebits/commons/bukkit/party/BukkitParty.java
+++ commons-bukkit/src/main/java/br/com/battlebits/commons/bukkit/party/BukkitParty.java
@@ -1,4 +1,47 @@
 package br.com.battlebits.commons.bukkit.party;
 
-public class BukkitParty {
+import br.com.battlebits.commons.party.Party;
+
+import java.util.UUID;
+
+public class BukkitParty extends Party {
+
+    public BukkitParty(UUID uuid) {
+        super(uuid);
+    }
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void onOwnerJoin() {
+
+    }
+
+    @Override
+    public void onOwnerLeave() {
+
+    }
+
+    @Override
+    public int getOnlineCount() {
+        return 0;
+    }
+
+    @Override
+    public void onMemberJoin(UUID member) {
+
+    }
+
+    @Override
+    public void onMemberLeave(UUID member) {
+
+    }
+
+    @Override
+    public void sendMessage(boolean prefix, boolean translate, String id, String[]... replacement) {
+
+    }
 }
