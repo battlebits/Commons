f73874fb20986af69ac6346c28a08dca198f0431
Chateado <chateadorn@gmail.com>
2019-08-05 16:07:35 -0300

    Added new translations

--- commons-core/src/main/java/br/com/battlebits/commons/translate/TranslateTag.java
+++ commons-core/src/main/java/br/com/battlebits/commons/translate/TranslateTag.java
@@ -28,22 +28,30 @@ public class TranslateTag {
     public final static String COMMAND_VANISH_PREFIX = "command.vanish.prefix";
     public final static String COMMAND_VANISH_INVISIBLE_GROUP = "command.vanish.invisible.group";
     public final static String COMMAND_VANISH_VISIBLE_ALL = "command.vanish.visible.all";
+    public final static String COMMAND_VANISH_RANK_NOT_EXIST = "command.vanish.rank.not.exist";
+    public final static String COMMAND_VANISH_RANK_HIGH = "command.vanish.rank.high";
+    public final static String COMMAND_VANISH_INVISIBLE = "command.vanish.invisible";
 
-    public final static String COMMAND_GROUPSET_PREFIX = "command.groupset-prefix";
-    public final static String COMMAND_GROUPSET_USAGE = "command.groupset-usage";
-    public final static String COMMAND_GROUPSET_GROUP_NOT_EXIST = "command.groupset-group-not-exist";
-    public final static String COMMAND_GROUPSET_NOT_ADMIN = "command.groupset-not-admin";
-    public final static String COMMAND_GROUPSET_ALREADY_IN_GROUP ="command.groupset-already-in-group";
-    public final static String COMMAND_GROUPSET_CHANGE_GROUP = "command.groupset-change-group";
+    public final static String COMMAND_GROUPSET_PREFIX = "command.groupset.prefix";
+    public final static String COMMAND_GROUPSET_USAGE = "command.groupset.usage";
+    public final static String COMMAND_GROUPSET_GROUP_NOT_EXIST = "command.groupset.group.not.exist";
+    public final static String COMMAND_GROUPSET_NOT_ADMIN = "command.groupset.not.admin";
+    public final static String COMMAND_GROUPSET_ALREADY_IN_GROUP ="command.groupset.already.in.group";
+    public final static String COMMAND_GROUPSET_CHANGE_GROUP = "command.groupset.change.group";
 
-    public final static String COMMAND_TAG_PREFIX = "command.tag-prefix";
-    public final static String COMMAND_TAG_AVAILABLE = "command.tag-available";
-    public final static String COMMAND_TAG_SELECT = "command.tag-select";
-    public final static String COMMAND_TAG_SELECTED = "command.tag-selected";
-    public final static String COMMAND_TAG_CURRENT = "command.tag-current";
-    public final static String COMMAND_TAG_NO_ACCESS = "command.tag-no-access";
-    public final static String COMMAND_TAG_NOT_FOUND = "command.tag-not-found";
-    public final static String COMMAND_TAG_NOT_ENABLED = "command.tag-not-enabled";
+    public final static String COMMAND_TAG_PREFIX = "command.tag.prefix";
+    public final static String COMMAND_TAG_AVAILABLE = "command.tag.available";
+    public final static String COMMAND_TAG_SELECT = "command.tag.select";
+    public final static String COMMAND_TAG_SELECTED = "command.tag.selected";
+    public final static String COMMAND_TAG_CURRENT = "command.tag.current";
+    public final static String COMMAND_TAG_NO_ACCESS = "command.tag.no.access";
+    public final static String COMMAND_TAG_NOT_FOUND = "command.tag.not.found";
+    public final static String COMMAND_TAG_NOT_ENABLED = "command.tag.not.enabled";
+
+    public final static String COMMAND_INVENTORYSEE_PREFIX = "command.inventorysee.prefix";
+    public final static String COMMAND_INVENTORYSEE_USAGE = "command.inventorysee.usage";
+    public final static String COMMAND_INVENTORYSEE_SUCCESS = "command.inventorysee.success";
+    public final static String COMMAND_INVENTORYSEE_NOT_FOUND = "command.inventorysee.not.found";
 
     /**
      * Server
