3c8cb6452d0f46377a55f1cf2d070a79751515c2
Gustavo Inacio <incio.gusta@gmail.com>
2019-08-15 00:26:46 -0300

    Added some translations and removed bold

--- commons-bukkit/src/main/java/br/com/battlebits/commons/bukkit/inventory/MenuAccount.java
+++ commons-bukkit/src/main/java/br/com/battlebits/commons/bukkit/inventory/MenuAccount.java
@@ -9,8 +9,8 @@ import br.com.battlebits.commons.bukkit.api.menu.MenuInventory;
 import br.com.battlebits.commons.bukkit.api.menu.MenuItem;
 import br.com.battlebits.commons.translate.Language;
 import br.com.battlebits.commons.util.DateUtils;
-import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
@@ -19,7 +19,9 @@ import org.bukkit.inventory.meta.SkullMeta;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 import static br.com.battlebits.commons.translate.TranslateTag.*;
@@ -33,37 +35,37 @@ public class MenuAccount extends MenuInventory {
         super(l.tl(MENU_ACCOUNT_TITLE, account.getName()), 6);
         accountUuid = account.getUniqueId();
         this.l = l;
-        ItemBuilder builder = ItemBuilder.create(Material.PLAYER_HEAD).name(account.getName());
+        ItemBuilder builder = ItemBuilder.create(Material.PLAYER_HEAD).name(ChatColor.GOLD + account.getName());
         ItemStack stack = builder.build();
         SkullMeta meta = (SkullMeta) stack.getItemMeta();
         meta.setOwningPlayer(Bukkit.getOfflinePlayer(account.getUniqueId()));
         stack.setItemMeta(meta);
-        MenuItem item = new MenuItem(stack);
-        setItem(13, item);
+        setItem(13, stack);
         String clanName = l.tl(MENU_ACCOUNT_DONT_HAVE_TEAM);
         if (account.getTeam() != null)
             clanName = account.getTeam().getName();
-        stack = ItemBuilder.create(Material.BLAZE_POWDER).name(ChatColor.GOLD + "" + ChatColor.BOLD + "Team").lore(l.tl(MENU_ACCOUNT_TEAM_NAME, clanName)).build();
+        stack = ItemBuilder.create(Material.BLAZE_POWDER).name(ChatColor.BLUE + "Team").lore(l.tl(MENU_ACCOUNT_TEAM_NAME, clanName)).build();
         setItem(19, stack);
-        stack = ItemBuilder.create(Material.GOLD_INGOT).name(ChatColor.GOLD + "" + ChatColor.BOLD + "BattleMoney").lore(ChatColor.GRAY + "" + account.getBattleMoney()).build();
+        stack = ItemBuilder.create(Material.GOLD_INGOT).name(ChatColor.GOLD + "BattleMoney").lore(ChatColor.GRAY + "" + account.getBattleMoney()).build();
         setItem(21, stack);
-        stack = ItemBuilder.create(Material.EMERALD).name(ChatColor.BOLD + "BattleCoins").lore(ChatColor.GRAY + "" + account.getBattleCoins()).build();
+        stack = ItemBuilder.create(Material.EMERALD).name(ChatColor.GREEN+ "BattleCoins").lore(ChatColor.GRAY + "" + account.getBattleCoins()).build();
         setItem(22, stack);
-        stack = ItemBuilder.create(Material.EXPERIENCE_BOTTLE).name(ChatColor.RED + "" + ChatColor.BOLD + "Xp").lore(ChatColor.GRAY + "" + account.getXp()).build();
+        stack = ItemBuilder.create(Material.EXPERIENCE_BOTTLE).name(ChatColor.DARK_PURPLE + "XP").lore(ChatColor.GRAY + "" + account.getXp()).build();
         setItem(23, stack);
         builder = ItemBuilder.create(Material.CLOCK).name(l.tl(MENU_ACCOUNT_TIME_INFO));
         Date date = new Date(account.getFirstTimePlaying());
         DateFormat df = new SimpleDateFormat("dd/MM/yyyy HH:mm:SS");
-        StringBuilder loreBuilder = new StringBuilder();
-        loreBuilder.append("\n" + l.tl(MENU_ACCOUNT_FIRST_LOGIN, df.format(date)));
+        List<String> loreList = new ArrayList<>();
+        loreList.add("");
+        loreList.add(l.tl(MENU_ACCOUNT_FIRST_LOGIN, df.format(date)));
         date = new Date(account.getLastLoggedIn());
-        loreBuilder.append("\n" + l.tl(MENU_ACCOUNT_LAST_LOGIN, df.format(date)));
-        loreBuilder.append("\n" + l.tl(MENU_ACCOUNT_TOTAL_ONLINE, DateUtils.formatDifference(l, account.getOnlineTime() / 1000)) + "\n");
+        loreList.add(l.tl(MENU_ACCOUNT_LAST_LOGIN, df.format(date)));
+        loreList.add(l.tl(MENU_ACCOUNT_TOTAL_ONLINE, DateUtils.formatDifference(l, account.getOnlineTime() / 1000)) + "\n");
         if (account.isOnline()) {
             date = new Date(System.currentTimeMillis() - account.getJoinTime());
-            loreBuilder.append("\n" + l.tl(MENU_ACCOUNT_CURRENT_ONLINE, DateUtils.formatDifference(l, (System.currentTimeMillis() - account.getJoinTime()) / 1000)));
+            loreList.add(l.tl(MENU_ACCOUNT_CURRENT_ONLINE, DateUtils.formatDifference(l, (System.currentTimeMillis() - account.getJoinTime()) / 1000)));
         }
-        builder.lore(loreBuilder.toString());
+        builder.lore(loreList);
         setItem(25, builder.build());
 
         setItem(49, ItemBuilder.create(Material.ENDER_EYE).name(l.tl(MENU_ACCOUNT_ACTUAL_GROUP, Tag.valueOf(account.getServerGroup().toString()).getPrefix())).build());
